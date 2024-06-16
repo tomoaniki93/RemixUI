82b48fa2c3e1b198421844b534a7824f47076032
Keintaro <43237397+tomoaniki93@users.noreply.github.com>
2024-06-16 06:34:10 +0200

    Add files via upload

---
+++ SharedMedia_MyMedia/MyMedia.lua
@@ -0,0 +1,57 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+LSM:Register("background", "Birg00", [[Interface\Addons\SharedMedia_MyMedia\background\Birg00.tga]]) 
+LSM:Register("background", "circle", [[Interface\Addons\SharedMedia_MyMedia\background\circle.tga]]) 
+LSM:Register("background", "circle1", [[Interface\Addons\SharedMedia_MyMedia\background\circle1.tga]]) 
+LSM:Register("background", "circle2", [[Interface\Addons\SharedMedia_MyMedia\background\circle2.tga]]) 
+LSM:Register("background", "circle3", [[Interface\Addons\SharedMedia_MyMedia\background\circle3.tga]]) 
+LSM:Register("background", "circle4", [[Interface\Addons\SharedMedia_MyMedia\background\circle4.tga]]) 
+LSM:Register("background", "Honeycomb", [[Interface\Addons\SharedMedia_MyMedia\background\Honeycomb.tga]]) 
+LSM:Register("background", "Honeycomb2", [[Interface\Addons\SharedMedia_MyMedia\background\Honeycomb2.tga]]) 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+LSM:Register("border", "circle1", [[Interface\Addons\SharedMedia_MyMedia\border\circle1.tga]]) 
+LSM:Register("border", "Honeycomb", [[Interface\Addons\SharedMedia_MyMedia\border\Honeycomb.tga]]) 
+LSM:Register("border", "Honeycomb2", [[Interface\Addons\SharedMedia_MyMedia\border\Honeycomb2.tga]]) 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "AdobeBlank", [[Interface\Addons\SharedMedia_MyMedia\font\AdobeBlank.ttf]]) 
+LSM:Register("font", "BebasNeue-Regular", [[Interface\Addons\SharedMedia_MyMedia\font\BebasNeue-Regular.ttf]]) 
+LSM:Register("font", "HomespunUPPERCASE", [[Interface\Addons\SharedMedia_MyMedia\font\HomespunUPPERCASE.ttf]]) 
+LSM:Register("font", "imagine_font", [[Interface\Addons\SharedMedia_MyMedia\font\imagine_font.ttf]]) 
+LSM:Register("font", "OblikCaps", [[Interface\Addons\SharedMedia_MyMedia\font\OblikCaps.ttf]]) 
+LSM:Register("font", "PTSans-NarrowUppercase", [[Interface\Addons\SharedMedia_MyMedia\font\PTSans-NarrowUppercase.ttf]]) 
+LSM:Register("font", "vibrocentric bd it", [[Interface\Addons\SharedMedia_MyMedia\font\vibrocentric bd it.ttf]]) 
+LSM:Register("font", "vibrocentric bd", [[Interface\Addons\SharedMedia_MyMedia\font\vibrocentric bd.ttf]]) 
+LSM:Register("font", "vibrocentric rg it", [[Interface\Addons\SharedMedia_MyMedia\font\vibrocentric rg it.ttf]]) 
+LSM:Register("font", "vibrocentric rg", [[Interface\Addons\SharedMedia_MyMedia\font\vibrocentric rg.ttf]]) 
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register("statusbar", "Birg00", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Birg00.tga]]) 
+LSM:Register("statusbar", "Birg01", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Birg01.tga]]) 
+LSM:Register("statusbar", "Birg02", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Birg02.tga]]) 
+LSM:Register("statusbar", "circle", [[Interface\Addons\SharedMedia_MyMedia\statusbar\circle.tga]]) 
+LSM:Register("statusbar", "circle1", [[Interface\Addons\SharedMedia_MyMedia\statusbar\circle1.tga]]) 
+LSM:Register("statusbar", "circle2", [[Interface\Addons\SharedMedia_MyMedia\statusbar\circle2.tga]]) 
+LSM:Register("statusbar", "circle3 1", [[Interface\Addons\SharedMedia_MyMedia\statusbar\circle3 1.tga]]) 
+LSM:Register("statusbar", "circle3 2", [[Interface\Addons\SharedMedia_MyMedia\statusbar\circle3 2.tga]]) 
+LSM:Register("statusbar", "circle3 3", [[Interface\Addons\SharedMedia_MyMedia\statusbar\circle3 3.tga]]) 
+LSM:Register("statusbar", "circle3 4", [[Interface\Addons\SharedMedia_MyMedia\statusbar\circle3 4.tga]]) 
+LSM:Register("statusbar", "circle4", [[Interface\Addons\SharedMedia_MyMedia\statusbar\circle4.tga]]) 
+LSM:Register("statusbar", "Honeycomb", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Honeycomb.tga]]) 
+LSM:Register("statusbar", "Honeycomb2", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Honeycomb2.tga]]) 
+LSM:Register("statusbar", "Ish00", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Ish00.tga]]) 
+LSM:Register("statusbar", "Square2", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Square2.tga]]) 
